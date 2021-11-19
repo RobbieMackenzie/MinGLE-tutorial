c3790b6caed046893be7008090b487eec5c924a1
Robbie Mackenzie <gm12172@bristol.ac.uk>
2021-11-19 14:52:22 +0000

    Have run and understood the batch version. Before tagging, I want to look in the Swan book to more proerly understand the syntax at play. The example creates a menu option in the GUI itself, which is cool

--- MinGLE/MinGLE-source/MinGLE.cc
+++ MinGLE/MinGLE-source/MinGLE.cc
@@ -1,7 +1,18 @@
-#include "G4UIExecutive.hh"
+#include <G4UIExecutive.hh>
+#include <G4UImanager.hh>
 
 int main(int argc, char **argv)
 {
-    G4UIExecutive ui(argc, argv);
-    ui.SessionStart();
+    if (argc == 1)
+    {
+        // Interactive mode
+        G4UIExecutive ui(argc, argv);
+        ui.SessionStart();
+    }
+    else
+    {
+        // Batch mode
+        G4String command = "/control/execute ";
+        G4UImanager::GetUIpointer()->ApplyCommand(command + argv[1]);
+    }
 }
